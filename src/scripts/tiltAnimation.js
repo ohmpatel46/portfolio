813a43eaffc690eaa872ea7548807d1d0d67f3ef
Ohm Patel <ohmpatel46@gmail.com>
2025-03-12 01:21:59 -0400

    ..

--- src/scripts/tiltAnimation.js
+++ src/scripts/tiltAnimation.js
@@ -1,4 +1,4 @@
-import VanillaTilt from "../../node_modules/vanilla-tilt/lib/vanilla-tilt";
+import VanillaTilt from "../../node_modules/vanilla-tilt/src/vanilla-tilt";
 
 export default function initTiltAnimation() {
   const elements = document.querySelectorAll(".js-tilt");
