1f5f96978c854e6cba2af261bafe6289d8845a95
Ohm Patel <ohmpatel46@gmail.com>
2025-03-12 01:55:53 -0400

    Using CDN

--- src/scripts/tiltAnimation.js
+++ src/scripts/tiltAnimation.js
@@ -1,6 +1,3 @@
-import VanillaTilt from "../../node_modules/vanilla-tilt/dist/vanilla-tilt";
-
-
 export default function initTiltAnimation() {
   const elements = document.querySelectorAll(".js-tilt");
   VanillaTilt.init(elements);
